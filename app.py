b492c55861d93870ab9ebf447626bd7a7832d0d5
Andrew Denisov <wolfhound97a@gmail.com>
2020-10-03 00:00:57 +0300

    add about.html

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def test():
     return "Hello World."
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route('/search')
 def search():
     return render_template("search.html")
